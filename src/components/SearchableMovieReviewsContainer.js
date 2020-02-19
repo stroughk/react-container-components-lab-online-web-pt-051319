33583496a53c3e06c14812fd29b3b4e43b88d35a
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-19 23:47:14 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,7 +6,6 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
 class SearchableMovieReviewsContainer extends Component {
   state = {
     searchTerm: '',
